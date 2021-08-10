bee0997c3877cb8385ba2ed8d6b161919f4cdc34
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-10 10:00:31 -0700

    fix: do not lowercase values

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function toValue(val) {
         return val;
     }
 
-    const value = val.toLowerCase().trim();
+    const value = val.trim();
     if (value === '_no response_') {
         return '';
     }
