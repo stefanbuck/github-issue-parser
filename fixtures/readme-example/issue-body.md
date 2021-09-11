6b2fb4e238e13057d9276a66073e6f3b9ef46b15
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-09-10 17:46:19 -0700

    test: README example

---
+++ fixtures/readme-example/issue-body.md
@@ -0,0 +1,20 @@
+### Your contact details
+
+me@me.com
+
+### What happened?
+
+A bug happened!
+
+### Version
+
+1.0.0
+
+### What browsers are you seeing the problem on?
+
+Chrome, Safari
+
+### Code of Conduct
+
+- [x] Never give up
+- [ ] Hot Dog is a Sandwich
--- test.spec.js
+++ test.spec.js
@@ -1,3 +1,62 @@
+const { readFileSync } = require("fs");
+
+const { run } = require(".");
+
 describe("Smoke test", () => {
-  it.todo("exports a run function");
+  it("exports a run function", () => {
+    expect(run).toBeDefined();
+    expect(typeof run).toBe("function");
+  });
+});
+
+describe("readme example", () => {
+  it("parses the issue body without a template", () => {
+    const expectedOutput = require("./fixtures/readme-example/expected.json");
+    const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
+
+    // mock ENV
+    const env = {
+      HOME: "<home path>",
+    };
+
+    // mock event payload
+    const eventPayload = require("./fixtures/readme-example/issue");
+
+    // mock fs
+    const fs = {
+      readFileSync(path, encoding) {
+        expect(path).toBe("<template-path>");
+        expect(encoding).toBe("utf8");
+        return readFileSync("fixtures/readme-example/form.yml", "utf-8");
+      },
+      writeFileSync(path, content) {
+        expect(path).toBe("<home path>/issue-parser-result.json");
+        expect(content).toBe(expectedOutputJson);
+      },
+    };
+
+    // mock core
+    const core = {
+      getInput(inputName) {
+        expect(inputName).toBe("template-path");
+        return "<template-path>";
+      },
+      setOutput(outputName, outputValue) {
+        if (outputName === "jsonString") {
+          expect(outputValue).toBe(expectedOutputJson);
+          return;
+        }
+
+        if (outputName.startsWith("issueparser_")) {
+          const key = outputName.substr("issueparser_".length);
+          expect(Object.keys(expectedOutput)).toContain(key);
+
+          expect(outputValue).toBe(expectedOutput[key]);
+          return;
+        }
+      },
+    };
+
+    run(env, eventPayload, fs, core);
+  });
 });
