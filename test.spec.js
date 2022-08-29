7da1c3a96e2657d61f8fdd6555ee31be7410e617
Jan Macku <jamacku@redhat.com>
2022-08-30 00:19:38 +0200

    fix: Handle correctly issues with an empty body (#34)
    * fix: Handle correctly issues with an empty body
    
    Fixes: #33
    
    * Update fixtures/blank/issue.js
    
    Co-authored-by: Gregor Martynus <39992+gr2m@users.noreply.github.com>
    
    Co-authored-by: Stefan Buck <github@stefanbuck.com>
    Co-authored-by: Gregor Martynus <39992+gr2m@users.noreply.github.com>

--- test.spec.js
+++ test.spec.js
@@ -106,3 +106,53 @@ it("multiple paragraphs", () => {
 
   run(env, eventPayload, fs, core);
 });
+
+it("blank", () => {
+  const expectedOutput = require("./fixtures/blank/expected.json");
+  const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
+
+  // mock ENV
+  const env = {
+    HOME: "<home path>",
+  };
+
+  // mock event payload
+  const eventPayload = require("./fixtures/blank/issue");
+
+  // mock fs
+  const fs = {
+    readFileSync(path, encoding) {
+      expect(path).toBe("<template-path>");
+      expect(encoding).toBe("utf8");
+      return readFileSync("fixtures/blank/form.yml", "utf-8");
+    },
+    writeFileSync(path, content) {
+      expect(path).toBe("<home path>/issue-parser-result.json");
+      expect(content).toBe(expectedOutputJson);
+    },
+  };
+
+  // mock core
+  const core = {
+    getInput(inputName) {
+      expect(inputName).toBe("template-path");
+      return "<template-path>";
+    },
+    setOutput(outputName, outputValue) {
+      if (outputName === "jsonString") {
+        expect(outputValue).toBe(expectedOutputJson);
+        return;
+      }
+
+      if (outputName.startsWith("issueparser_")) {
+        const key = outputName.substr("issueparser_".length);
+        expect(Object.keys(expectedOutput)).toContain(key);
+
+        expect(outputValue).toBe(expectedOutput[key]);
+        return;
+      }
+    },
+  };
+
+  run(env, eventPayload, fs, core);
+});
