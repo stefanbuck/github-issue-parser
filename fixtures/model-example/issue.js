979a817c19ed26701e47696d159f3368c3993ff2
Grant Birkinbine <grantbirki@github.com>
2022-10-03 16:04:52 -0600

    ersilia model tests

---
+++ fixtures/model-example/issue.js
@@ -0,0 +1,10 @@
+// const { resolve } = require("path");
+// const { readFileSync } = require("fs");
+
+// const issueBodyPath = resolve(__dirname, "issue-body.md");
+
+module.exports = {
+  issue: {
+    body: "### Model Name\n\ncool molecule\n\n### Model Description\n\nThis is a prediction for a super cool molecule\n\n### Ersilia ID\n\neos11aa\n\n### Publication\n\nThe following link is just an example:\r\n\r\nwww.example.com\n\n### Code\n\n_No response_\n\n### License\n\n_No response_",
+  },
+};
--- test.spec.js
+++ test.spec.js
@@ -57,6 +57,56 @@ it("readme example", () => {
   run(env, eventPayload, fs, core);
 });
 
+it("model example", () => {
+  const expectedOutput = require("./fixtures/model-example/expected.json");
+  const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
+
+  // mock ENV
+  const env = {
+    HOME: "<home path>",
+  };
+
+  // mock event payload
+  const eventPayload = require("./fixtures/model-example/issue");
+
+  // mock fs
+  const fs = {
+    readFileSync(path, encoding) {
+      expect(path).toBe("<template-path>");
+      expect(encoding).toBe("utf8");
+      return readFileSync("fixtures/model-example/form.yml", "utf-8");
+    },
+    writeFileSync(path, content) {
+      expect(path).toBe("<home path>/issue-parser-result.json");
+      expect(content).toBe(expectedOutputJson);
+    },
+  };
+
+  // mock core
+  const core = {
+    getInput(inputName) {
+      expect(inputName).toBe("template-path");
+      return "<template-path>";
+    },
+    setOutput(outputName, outputValue) {
+      if (outputName === "jsonString") {
+        expect(outputValue).toBe(expectedOutputJson);
+        return;
+      }
+
+      if (outputName.startsWith("issueparser_")) {
+        const key = outputName.substr("issueparser_".length);
+        expect(Object.keys(expectedOutput)).toContain(key);
+
+        expect(outputValue).toBe(expectedOutput[key]);
+        return;
+      }
+    },
+  };
+
+  run(env, eventPayload, fs, core);
+});
+
 it("multiple paragraphs", () => {
   const expectedOutput = require("./fixtures/multiple-paragraphs/expected.json");
   const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
