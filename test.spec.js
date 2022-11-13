8f164f896b07796ef8ccf97f3310d21da45cf394
GrantBirki <grantbirki@github.com>
2022-11-13 16:23:21 +0000

    updated tests

--- test.spec.js
+++ test.spec.js
@@ -22,3 +22,14 @@ it("multi paragraph", async () => {
 
   expect(jsonDict).toEqual(expectedOutputJson);
 });
+
+it("model example raw string", async () => {
+  const expectedOutput = require("./fixtures/model-example/expected.json");
+  const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
+
+  const body = "### Model Name\r\n\r\nmy cool molecule\r\n\r\n### Model Description\r\n\r\nThis is a prediction for a super cool molecule\r\n\r\n### Ersilia ID\r\n\r\neos11aa\r\n\r\n### Publication\r\n\r\nThe following link is just an example:\r\n\r\nwww.example.com\r\n\r\n### Code\r\n\r\n_No response_\r\n\r\n### License\r\n\r\n_No response_"
+
+  const jsonDict = await parse(body)
+
+  expect(jsonDict).toEqual(expectedOutputJson);
+});
