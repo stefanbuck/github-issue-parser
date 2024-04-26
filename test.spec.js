b6c8f2cacb09bd2186df2101c548683bf28f0469
Christian Marangi <ansuelsmth@gmail.com>
2024-04-26 18:57:19 +0200

    feat: add additional test for section with ####
    Add additional test for section with #### to prevent and catch in
    future code change that would produce wrong parsing.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- test.spec.js
+++ test.spec.js
@@ -174,6 +174,45 @@ it("multiple paragraphs", () => {
   expect(core.setOutput.mock.calls.length).toBe(3)
 });
 
+it("paragraph with confusing ####", () => {
+  const expectedOutput = require("./fixtures/paragraph-confusing-####/expected.json");
+  const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
+
+  // mock ENV
+  const env = {
+    HOME: "<home path>",
+  };
+
+  // mock event payload
+  const eventPayload = require("./fixtures/paragraph-confusing-####/issue");
+
+  // mock fs
+  const fs = {
+    readFileSync(path, encoding) {
+      expect(path).toBe("<template-path>");
+      expect(encoding).toBe("utf8");
+      return readFileSync("fixtures/paragraph-confusing-####/form.yml", "utf-8");
+    },
+    writeFileSync(path, content) {
+      expect(path).toBe("<home path>/issue-parser-result.json");
+      expect(content).toBe(expectedOutputJson);
+    },
+  };
+
+  // mock core
+  const core = {
+    getInput: jest.fn(() => '<template-path>'),
+    setOutput: jest.fn(),
+  };
+
+  run(env, eventPayload, fs, core);
+
+  expect(core.getInput).toHaveBeenCalledWith('template-path')
+  expect(core.setOutput).toHaveBeenCalledWith('jsonString', JSON.stringify(expectedOutput, null, 2))
+  expect(core.setOutput).toHaveBeenCalledWith('issueparser_textarea-one', 'Textarea input text 1 ####')
+  expect(core.setOutput.mock.calls.length).toBe(2)
+});
+
 it("blank", () => {
   const expectedOutput = require("./fixtures/blank/expected.json");
   const expectedOutputJson = JSON.stringify(expectedOutput, null, 2);
